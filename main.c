85036a5a0878a2b3da026da6622f4ad56b1f2189
jlin03 <43045402+jlin03@users.noreply.github.com>
2019-12-15 18:21:23 -0500

    final

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "headers.h"
+
+
+int main() {
+  shell();
+}
